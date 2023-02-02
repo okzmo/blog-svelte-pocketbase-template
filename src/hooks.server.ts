1ba5fd82a85d972d6a31ed89a421c0600d26c12f
Mind-thatsall <mathieurossi77@gmail.com>
2023-02-02 12:16:16 +0100

    Initialization of the project

---
+++ src/hooks.server.ts
@@ -0,0 +1,26 @@
+import { pb } from '$lib/pocketbase';
+import type { Handle } from '@sveltejs/kit';
+
+export const handle: Handle = async ({event, resolve}) => {
+    // Load the cookie if any
+    pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
+
+    // Check if the token is valid
+    if(pb.authStore.isValid) {
+        try {
+            await pb.collection('users').authRefresh()
+        } catch(_) {
+            pb.authStore.clear();
+        }
+    }
+
+    // Context to pass data (to our backend handlers)
+    event.locals.pb = pb;
+    event.locals.user = structuredClone(pb.authStore.model);
+
+    const response = await resolve(event);
+
+    response.headers.set('set-cookie', pb.authStore.exportToCookie());
+
+    return response;
+}
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,8 @@
+<div class="flex items-center justify-center h-screen">
+    <h1>Log In</h1>
+    <form action="" class="flex flex-col justify-center bg-slate-700 p-6 items-center w-2/5 h-2/5">
+        <input type="email" class="w-2/3 mb-4 p-2"/>
+        <input type="password" class="w-2/3 mb-4 p-2" />
+        <button type='submit' class="w-1/5 rounded-md bg-slate-500 p-2">Login</button>
+    </form>
+</div>
