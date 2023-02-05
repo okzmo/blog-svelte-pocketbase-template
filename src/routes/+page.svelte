2299f4c1d3f52fa85f89327079593c073cedec74
Mind-thatsall <mathieurossi77@gmail.com>
2023-02-05 18:12:33 +0100

    added: more styling on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,44 +4,89 @@
 	import { currentUser, pb } from '$lib/pocketbase';
 </script>
 
-<main class="flex flex-col lg:flex-row overflow-hidden">
-	<section class="w-full lg:w-1/2 h-screen border-r-2 border-black flex flex-col z-[2]">
-		<div class="h-1/6 border-b-2 border-black flex items-center">
-			<h1 class="text-6xl md:text-[10vw] lg:text-[5vw] pl-6 font-bold bg-[#d6d1b7] z-[2]">
-				<span>MORE</span>
-				<span class="ml-2">DESIGN</span>
+<main class="flex flex-col lg:flex-row overflow-hidden border-[0.5vh] border-black lg:h-screen">
+	<section
+		class="w-full lg:w-1/2 h-fit lg:h-full lg:border-r-[0.5vh] border-black flex flex-col z-[2]"
+	>
+		<div
+			class="h-fit lg:h-[15vh] border-b-[0.5vh] border-black flex justify-center lg:justify-start items-center bg-[#d6d1b7]"
+		>
+			<h1 class="text-[13vw] md:text-[10vw] lg:text-[5vw] lg:pl-[2.5vh] font-bold z-[2]">
+				{#if $currentUser}
+					HEY, USERNAME
+				{:else}
+					HELLO, THERE!
+				{/if}
 			</h1>
 		</div>
-		<div class="h-5/6 flex">
-			<div class="hidden w-1/3 border-r-2 border-black lg:flex items-end justify-center">
+		<div class="h-fit lg:h-[85vh] flex">
+			<div class="hidden w-1/3 border-r-[0.5vh] border-black lg:flex items-end justify-center">
 				<p class="md:text-[4.5vw] font-bold">ART.01</p>
 			</div>
-			<div class="w-full lg:w-2/3 flex flex-col">
-				<img
-					src="https://images.unsplash.com/photo-1675430420657-72694b2f66be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format"
-					alt=""
-					class="h-[45vh] lg:h-[20vw] w-full mb-5 px-[1.2vw] pt-[1.2vw]"
-				/>
-				<div class="px-6 pb-6">
-					<h2 class="text-3xl xl:text-5xl mb-4 font-medium">What's new in TailwindCSS V2</h2>
-					<span class="flex justify-between mb-3">
-						<p>By <span class="font-bold">Mathieu Rossi</span></p>
-						<p>5 min Read</p>
-					</span>
-					<p class="text-md xl:text-[1.3vw] font-light">
-						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur sapiente
-						reiciendis voluptatum assumenda, quae iure! Placeat neque corporis asperiores ducimus
-						non nam facilis reprehenderit, tempore, commodi, iste nostrum a distinctio!
-					</p>
+			<div class="w-full lg:w-2/3 overflow-auto">
+				<div class="flex flex-col border-b-[0.5vh] border-black h-fit lg:h-full">
+					<div class="h-fit sm:h-[90%]">
+						<img
+							src="https://images.unsplash.com/photo-1675430420657-72694b2f66be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format"
+							alt=""
+							class="h-[40vh] lg:h-[38vh] w-full mb-5 lg:mb-[1.4vh] px-4 pt-4 lg:px-[1.2vw] lg:pt-[1.2vw]"
+						/>
+						<div class="px-5 pb-5 lg:px-[1.2vw] lg:pb-[1.2vw]">
+							<h2 class="text-3xl md:text-[3.5vw] lg:leading-[3vw] mb-4 lg:mb-[1.2vh] font-bold">
+								What's new in TailwindCSS V2
+							</h2>
+							<span class="flex justify-between mb-3 lg:mb-[1vh]">
+								<p class="md:text-[0.86vw]">By <span class="font-bold">Mathieu Rossi</span></p>
+								<p class="md:text-[0.86vw]">5 min Read</p>
+							</span>
+							<p class="text-md lg:text-[1.73vw] xl:text-[2.4vh] font-regular">
+								Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur sapiente
+								reiciendis voluptatum assumenda, quae iure! Placeat neque corporis asperiores
+								ducimus non nam facilis reprehenderit, tempore, commodi, iste nostrum a distinctio!
+							</p>
+						</div>
+					</div>
+					<div
+						class="hidden lg:flex justify-end items-center border-t-[0.5vh] border-black h-[10%]"
+					>
+						<a href="/article" class="text-[2.2vh] px-[1.2vw]">Read More</a>
+					</div>
 				</div>
-				<div class="hidden lg:flex justify-end items-center border-t-2 border-black">
-					<a href="/article" class="text-2xl px-6 py-4">Read More</a>
+				<div class="flex flex-col border-b-[0.5vh] border-black h-fit lg:h-full">
+					<div class="h-fit sm:h-[90%]">
+						<img
+							src="https://images.unsplash.com/photo-1675430420657-72694b2f66be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format"
+							alt=""
+							class="h-[40vh] lg:h-[38vh] w-full mb-5 lg:mb-[1.4vh] px-4 pt-4 lg:px-[1.2vw] lg:pt-[1.2vw]"
+						/>
+						<div class="px-5 pb-5 lg:px-[1.2vw] lg:pb-[1.2vw]">
+							<h2 class="text-3xl md:text-[3.5vw] lg:leading-[3vw] mb-4 lg:mb-[1.2vh] font-bold">
+								What's new in TailwindCSS V2
+							</h2>
+							<span class="flex justify-between mb-3 lg:mb-[1vh]">
+								<p class="md:text-[0.86vw]">By <span class="font-bold">Mathieu Rossi</span></p>
+								<p class="md:text-[0.86vw]">5 min Read</p>
+							</span>
+							<p class="text-md lg:text-[1.2vw] font-regular">
+								Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur sapiente
+								reiciendis voluptatum assumenda, quae iure! Placeat neque corporis asperiores
+								ducimus non nam facilis reprehenderit, tempore, commodi, iste nostrum a distinctio!
+							</p>
+						</div>
+					</div>
+					<div
+						class="hidden lg:flex justify-end items-center border-t-[0.5vh] border-black h-[10%]"
+					>
+						<a href="/article" class="text-[2.2vh] px-[1.2vw]">Read More</a>
+					</div>
 				</div>
 			</div>
 		</div>
 	</section>
 	<section class="w-full lg:w-1/2 flex flex-col">
-		<div class="flex gap-2 justify-end items-center border-b-2 border-black py-4 px-6">
+		<div
+			class="hidden lg:flex h-[8vh] gap-2 justify-end items-center border-b-[0.5vh] border-black py-4 lg:py-[2vh] px-6 lg:px-[2vh]"
+		>
 			{#if $currentUser}
 				<form
 					action="/logout"
@@ -58,28 +103,28 @@
 			{:else}
 				<a
 					href="/login"
-					class="border-2 border-black bg-black text-[#d6d1b7] px-8 py-3 text-sm hover:bg-[#d6d1b7] hover:text-black transition"
+					class="border-[0.5vh] border-black bg-black text-[#d6d1b7] px-8 py-3 lg:px-[2.2vw] lg:py-[1.2vh] text-sm lg:text-[0.8vw] lg:leading-[2vh] hover:bg-[#d6d1b7] hover:text-black transition"
 					>LOGIN</a
 				>
 				<a
 					href="/register"
-					class="border-2 border-black px-8 py-3 text-sm hover:bg-black hover:text-[#d6d1b7] transition"
+					class="border-[0.5vh] border-black px-8 py-3 lg:px-[2.2vw] lg:py-[1.2vh] text-sm lg:text-[0.8vw] lg:leading-[2vh] hover:bg-black hover:text-[#d6d1b7] transition"
 					>REGISTER</a
 				>
 			{/if}
 		</div>
-		<div class="h-2/5 flex flex-col pt-6 border-b-2 border-black justify-between">
-			<div class="bg-black text-[#d6d1b7] text-2xl whitespace-nowrap flex">
-				<div class="txt text-3xl">
+		<div class="h-fit lg:h-[30vh] flex flex-col pt-6 border-b-[0.5vh] border-black justify-between">
+			<div class="bg-black text-[#d6d1b7] text-[3vh] py-[0.5vh] whitespace-nowrap hidden lg:flex">
+				<div class="txt ">
 					New Article - New Article - New Article - New Article - New Article - New Article -&nbsp;
 				</div>
-				<div class="txt text-3xl">
+				<div class="txt">
 					New Article - New Article - New Article - New Article - New Article - New Article -&nbsp;
 				</div>
 			</div>
-			<h3>New Videos</h3>
+			<h3>Latest Projects</h3>
 		</div>
-		<div class="h-3/5">
+		<div class="lg:h-[62vh]">
 			<h2>How did I make this blog using Svelte and Pocketbase</h2>
 			<p>
 				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error velit sint, officiis atque
